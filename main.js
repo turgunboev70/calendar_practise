82e1504d346b62013e102223efdfc744c06f4f0f
turgunboev70 <turgunboevjr@gmail.com>
2022-10-11 02:10:38 +0500

    calendar

---
+++ main.js
@@ -0,0 +1,53 @@
+const currentDate = document.querySelector(".current-date")
+daysTag = document.querySelector(".days")
+prevNextIcon = document.querySelectorAll(".icons span")
+
+
+let date = new Date()
+currYear = date.getFullYear(),
+    currMonth = date.getMonth()
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+
+const renderCalendar = () => {
+    let firstDayOfMonth = new Date(currYear, currMonth + 1).getDay() // getting first day of month
+    let lastDateOfMonth = new Date(currYear, currMonth + 1, 0).getDate() // geeting last date of month
+    let lastDayOfMonth = new Date(currYear, currMonth, lastDateOfMonth).getDay() // getting last day of month
+    let lastDateOfLastMonth = new Date(currYear, currMonth, 0).getDate() // getting last date of previous month
+    let liTag = ""
+
+    for (let i = firstDayOfMonth; i > 0; i--) { // creating li of previous month last days
+        liTag += `<li class="inactive">${lastDateOfLastMonth - i + 1}</li>`
+    }
+
+    for (let i = 1; i <= lastDateOfMonth; i++) { // creating li of all days of current month
+        // adding active class to li if the current day, month, and year matched
+        let isToday = i === date.getDate() && currMonth === new Date().getMonth()
+                        && currYear === new Date().getFullYear() ? "active" : ""
+        liTag += `<li class="${isToday}">${i}</li>`
+    }
+    for (let i = lastDayOfMonth; i < 6; i++) { // creating li of next month first days
+        liTag += `<li class="inactive">${i - lastDayOfMonth + 1}</li>`
+        
+    }
+
+    currentDate.innerText = `${months[currMonth]} ${currYear}`
+    daysTag.innerHTML = liTag
+}
+
+renderCalendar()
+
+prevNextIcon.forEach(icon => {
+    icon.addEventListener('click', () => {
+        currMonth = icon.id === "prev" ? currMonth - 1 : currMonth + 1
+
+        if (currMonth < 0 || currMonth > 11) { // if current month is less than 0 or greater than 11
+            // creating a new date of current year & month and pass it as date value
+            date = new Date(currYear, currMonth) 
+            currYear = date.getFullYear() // updating current year with new date year
+            currMonth = date.getMonth() // updating current month with new date month
+        } else { // else pass new Date as date value 
+            date = new Date()
+        }
+        renderCalendar()
+    })
+});
